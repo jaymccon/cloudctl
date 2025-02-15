e4d435c88b4c5cad293de4b3e49f9a3651561f53
Jay McConnell <jmmccon@amazon.com>
2025-02-15 21:20:25 +0000

    initial cpp setup

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+double square(double x) {
+    return x * x;
+}
+
+void print_square(double x) {
+    cout << "the square of " << x << " is " << square(x) << "\n";
+}
+
+int main() {
+    cout << "Hello, Wayne's World! \n";
+    print_square(1.234);
+    return 0;
+}
